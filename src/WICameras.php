8bcfbe941a9cef7317283079c15e7d22eae56e4f
ruscoe <danruscoe@protonmail.com>
2026-08-17 18:32:06 -0500

    Add function to get a specific camera.

--- src/WICameras.php
+++ src/WICameras.php
@@ -23,4 +23,27 @@ class WICameras extends WIAPI
     {
         return $this->request('GET', '/get/cameras');
     }
+
+    /**
+     * Gets a specific camera.
+     *
+     * @param int $id the camera ID
+     *
+     * @return object
+     *
+     * @see https://511wi.gov/help/endpoint/cameras
+     */
+    public function getCamera($id)
+    {
+        $cameras = $this->request('GET', '/get/cameras');
+
+        $total_cameras = count($cameras);
+        for ($i = 0; $i < $total_cameras; $i++) {
+            if ($cameras[$i]->Id == $id) {
+                return $cameras[$i];
+            }
+        }
+
+        return null;
+    }
 }
